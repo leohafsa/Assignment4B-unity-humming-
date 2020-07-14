d4d7efcb137cb99824ea70a8137d96201fcd48d2
leohafsa <fa19-rcs-021@student.comsats.edu.pk>
2020-07-14 22:57:28 +0500

    done
    done

---
+++ HummingBirds/Assets/Scripts/menuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour
+{
+   public void playPenguin()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+    public void playHummingbird()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+}
